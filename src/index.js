6e0ebbd7a9e5657a9b9a0665df25ac2c68e0200e
falsy <me@falsy.me>
2019-09-01 22:42:59 +0900

    기본 프로미스 함수 추가 및 로그 메세지 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import Buffer from './Buffer';
 const request1 = () => {
   return new Promise(resolve => {
     setTimeout(() => {
+      console.log('request1 start');	
       resolve('ok');
     }, 1000);
   });
@@ -10,6 +11,7 @@ const request1 = () => {
 const request2 = () => {
   return new Promise(resolve => {
     setTimeout(() => {
+      console.log('request2 start');
       resolve('ok2');
     }, 3000);
   });
@@ -17,6 +19,7 @@ const request2 = () => {
 const request3 = () => {
   return new Promise(resolve => {
     setTimeout(() => {
+      console.log('request3 start');
       resolve('ok3');
     }, 1000);
   });
@@ -24,6 +27,7 @@ const request3 = () => {
 const request4 = () => {
   return new Promise(resolve => {
     setTimeout(() => {
+      console.log('request4 start');
       resolve('ok4');
     }, 3000);
   });
@@ -43,7 +47,35 @@ Buffer(request4).then((res) => {
 });
 
 
+const req1 = Buffer(request1);
+const req2 = Buffer(request2);
+const req3 = Buffer(request3);
+const req4 = Buffer(request4);
+
+
+const bPromise1 = () => {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			console.log('promise start1');
+			resolve('promise ok1');
+		}, 2000)
+	});
+} 
+
+const bPromise2 = () => {
+	return new Promise(resolve => {
+		setTimeout(() => {
+			console.log('promise start2');
+			resolve('promise ok2');
+		}, 1000)
+	});
+}
+
+const pro1 = bPromise1();
+const pro2 = bPromise2();
+
+
 (async () => {
   const abc = await Buffer(request4).then(res => res);
-  console.log(abc);
+  console.log('async-'+abc);
 })();
