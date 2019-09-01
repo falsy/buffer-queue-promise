f79c83ff894e4f7d504f450c917a0539af378b83
falsy <me@falsy.me>
2019-09-02 00:43:55 +0900

    Update Buffer.js

--- src/Buffer.js
+++ src/Buffer.js
@@ -3,7 +3,7 @@ import Queue from './Queue.js';
 export default (request) => {
   return new Promise(resolve => {
     Queue.enqueue(() => {
-      return new Promise((innerResolve, innerInject) => {
+      return new Promise(innerResolve => {
         request().then(res => innerResolve(res));
       });
     }, res => resolve(res));
