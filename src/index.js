311b9b963398889623030639758b37f0d6b10717
falsy <me@falsy.me>
2019-09-01 23:41:55 +0900

    예시 코드 수정

--- src/index.js
+++ src/index.js
@@ -5,77 +5,37 @@ const request1 = () => {
     setTimeout(() => {
       console.log('request1 start');	
       resolve('ok');
-    }, 1000);
+    }, 2000);
   });
 };
 const request2 = () => {
   return new Promise(resolve => {
     setTimeout(() => {
       console.log('request2 start');
-      resolve('ok2');
-    }, 3000);
-  });
-};
-const request3 = () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      console.log('request3 start');
-      resolve('ok3');
+      resolve('ok');
     }, 1000);
   });
 };
-const request4 = () => {
+const basicRequest1 = () => {
   return new Promise(resolve => {
     setTimeout(() => {
-      console.log('request4 start');
-      resolve('ok4');
-    }, 3000);
+      console.log('basic request1 start');
+      resolve('ok');
+    }, 2000);
+  });
+};
+const basicRequest2 = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('basic requset2 start');
+      resolve('ok');
+    }, 1000);
   });
 };
 
-Buffer(request1).then((res) => {
-  console.log(res);
-});
-Buffer(request2).then((res) => {
-  console.log(res);
-});
-Buffer(request3).then((res) => {
-  console.log(res);
-});
-Buffer(request4).then((res) => {
-  console.log(res);
-});
 
+const pro1 = basicRequest1();
+const pro2 = basicRequest2();
 
 const req1 = Buffer(request1);
 const req2 = Buffer(request2);
-const req3 = Buffer(request3);
-const req4 = Buffer(request4);
-
-
-const bPromise1 = () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      console.log('promise start1');
-      resolve('promise ok1');
-    }, 2000)
-  });
-} 
-
-const bPromise2 = () => {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      console.log('promise start2');
-      resolve('promise ok2');
-    }, 1000)
-  });
-}
-
-const pro1 = bPromise1();
-const pro2 = bPromise2();
-
-
-(async () => {
-  const abc = await Buffer(request4).then(res => res);
-  console.log('async-'+abc);
-})();
